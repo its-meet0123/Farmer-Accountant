590ef3547a9846f8c0be99bf436fb2a59c7ad785
its-meet0123 <sukh.meet9896@gmail.com>
2026-02-15 15:16:05 +0530

    changes in main.jsx

--- farmer-heplers/src/main.jsx
+++ farmer-heplers/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./app/auth/AuthContext.jsx";
+import "./i18n.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
