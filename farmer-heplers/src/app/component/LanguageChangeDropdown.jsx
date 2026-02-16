14361508ad80edaa802f610175220dd653c04a76
its-meet0123 <sukh.meet9896@gmail.com>
2026-02-16 10:17:06 +0530

    set triple language system

--- farmer-heplers/src/app/component/LanguageChangeDropdown.jsx
+++ farmer-heplers/src/app/component/LanguageChangeDropdown.jsx
@@ -3,16 +3,16 @@ import { DownOutlined, SmileOutlined } from "@ant-design/icons";
 import { Button, Dropdown, Space } from "antd";
 import i18n from "i18next";
 const LanguageChangeDropDown = () => {
-  const [buttonText, setButtonText] = useState("Eng");
+  const [buttonText, setButtonText] = useState("English");
   const items = [
     {
       key: "1",
       label: (
         <a
           onClick={() => {
-            (i18n.changeLanguage("en"), setButtonText("Eng"));
+            (i18n.changeLanguage("en"), setButtonText("English"));
           }}>
-          En
+          English
         </a>
       ),
     },
@@ -21,16 +21,29 @@ const LanguageChangeDropDown = () => {
       label: (
         <a
           onClick={() => {
-            (i18n.changeLanguage("hi"), setButtonText("Hin"));
+            (i18n.changeLanguage("hi"), setButtonText("हिंदी"));
           }}>
-          Hi
+          हिंदी
+        </a>
+      ),
+    },
+    {
+      key: "3",
+      label: (
+        <a
+          onClick={() => {
+            (i18n.changeLanguage("pu"), setButtonText("ਪੰਜਾਬੀ"));
+          }}>
+          ਪੰਜਾਬੀ
         </a>
       ),
     },
   ];
   return (
     <Dropdown menu={{ items }}>
-      <Button>{buttonText}</Button>
+      <Button color="default" variant="solid">
+        {buttonText}
+      </Button>
     </Dropdown>
   );
 };
--- farmer-heplers/src/i18n.js
+++ farmer-heplers/src/i18n.js
@@ -2,11 +2,13 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import en from "./locales/en.json";
 import hi from "./locales/hi.json";
+import pu from "./locales/pu.json";
 
 i18n.use(initReactI18next).init({
   resources: {
     en: { translation: en },
     hi: { translation: hi },
+    pu: { translation: pu },
   },
   lng: "en",
   fallbackLng: "en",
