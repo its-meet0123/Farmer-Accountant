eaf5529042381d8d155cf807ae8eb9735858a944
its-meet0123 <sukh.meet9896@gmail.com>
2026-02-16 08:42:19 +0530

    npm install

--- farmer-heplers/src/app/constant/layout.js
+++ farmer-heplers/src/app/constant/layout.js
@@ -1,19 +1,2 @@
-import { useTranslation } from "react-i18next";
-function getItem(label, key, children, icon) {
-  const { t } = useTranslation();
-  return {
-    label: t(label),
-    key,
-    children,
-    icon,
-  };
-}
 
-export const primeryMenu = [getItem(`Home`, "/home"), getItem(`View`, "/view")];
-export const moreMenu = [
-  getItem("Worker", "/worker"),
-  getItem("Setting", "/setting", [
-    getItem("Logout", "/logout"),
-    getItem("Delete", "/delete"),
-  ]),
-];
+
