e36ef2762cee7fb256c9607ebb4019c004e42faf
its-meet0123 <sukh.meet9896@gmail.com>
2026-02-19 09:20:05 +0530

    token clear concept issue solve try 1

--- helper-engine/controllers/user.js
+++ helper-engine/controllers/user.js
@@ -107,6 +107,7 @@ async function handleUserLogOut(req, res) {
       secure: true,
       sameSite: "none",
       path: "/",
+      expires: new Date(0),
     })
     .json({
       status: "success",
