5b2622a7e021e57d518d9301c45c5b012eabd920
its-meet0123 <sukh.meet9896@gmail.com>
2026-03-01 15:21:12 +0530

    changes in backend inddata delete functions

--- helper-engine/controllers/integratedData.js
+++ helper-engine/controllers/integratedData.js
@@ -172,11 +172,11 @@ async function handleDeleteManyIndData(req, res) {
     if (!indDataById) {
       return res
         .status(404)
-        .json({ status: "Error", msg: "Ind Data not found" });
+        .json({ status: "Error", message: "Ind Data not found" });
     }
     return res
       .status(200)
-      .json({ status: "Success", msg: "Ind. Datas Deleted successfully" });
+      .json({ status: "Success", message: "Ind. Datas Deleted successfully" });
   } catch (err) {
     return res.status(500).json({ status: "Error", message: err.message });
   }
