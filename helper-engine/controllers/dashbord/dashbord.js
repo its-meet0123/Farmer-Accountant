71a10f21ddf6e6233064d1c327a55ffa4d2ff6b6
its-meet0123 <sukh.meet9896@gmail.com>
2026-03-27 17:15:06 +0530

    deshbord set try2

--- helper-engine/controllers/dashbord/dashbord.js
+++ helper-engine/controllers/dashbord/dashbord.js
@@ -19,64 +19,77 @@ const {
 // Dashboard controller functions using map method
 
 async function dashBordData(req, res) {
-  const token = req.cookies.token;
-  const decoded = jwt.verify(token, JWT_SECRET);
-  const currentUserId = decoded.id;
-  const Ind = await Industries.find({ userId: currentUserId });
+  try {
+    const token = req.cookies.token;
+    const decoded = jwt.verify(token, JWT_SECRET);
+    const currentUserId = decoded.id;
+    const Ind = await Industries.find({ userId: currentUserId });
 
-  const allShopes = Ind.flatMap((shopes) => {
-    const shopeNumber = shopes?.shopeNumber;
-    const shopeData = shopes.shopeAccount.map((transaction) => {
-      const startDate = transaction?.startDate || "";
-      const rate = transaction?.rate || 24;
-      const endDate = new Date();
-      const loanAmount = transaction?.loan?.amount || 0;
-      //loan
-      const loanInterest = calculateAutoInterestForTakeAmount(
-        loanAmount,
-        startDate,
-        rate,
-        endDate,
-      );
-      //buy item
-      const buyAmount = transaction?.indBuy?.billAmount || 0;
-      const buyInterest = calculateAutoInterestForBuyBillAmount(
-        buyAmount,
-        startDate,
-        rate,
-        endDate,
-      );
-      //sellItem
-      const sellAmount = transaction?.indSell?.billAmount || 0;
-      const sellInterest = calculateAutoInterestForGiveAmount(
-        sellAmount,
-        startDate,
-        rate,
-        endDate,
-      );
-      //dieselAmount
-      const dieselAmount = transaction?.diesel?.billAmount || 0;
-      const dieselInterest = calculateAutoInterestDieselBillAmount(
-        dieselAmount,
-        startDate,
-        rate,
-        endDate,
-      );
+    const allShopes = Ind.flatMap((shopes) => {
+      const shopeNumber = shopes?.shopeNumber;
+      const shopeData = shopes.shopeAccount.map((transaction) => {
+        const startDate = transaction?.startDate || "";
+        const rate = transaction?.rate || 24;
+        const endDate = new Date();
+        const loanAmount = transaction?.loan?.amount || 0;
+        //loan
+        const loanInterest = calculateAutoInterestForTakeAmount(
+          loanAmount,
+          startDate,
+          rate,
+          endDate,
+        );
+        //buy item
+        const buyAmount = transaction?.indBuy?.billAmount || 0;
+        const buyInterest = calculateAutoInterestForBuyBillAmount(
+          buyAmount,
+          startDate,
+          rate,
+          endDate,
+        );
+        //sellItem
+        const sellAmount = transaction?.indSell?.billAmount || 0;
+        const sellInterest = calculateAutoInterestForGiveAmount(
+          sellAmount,
+          startDate,
+          rate,
+          endDate,
+        );
+        //dieselAmount
+        const dieselAmount = transaction?.diesel?.billAmount || 0;
+        const dieselInterest = calculateAutoInterestDieselBillAmount(
+          dieselAmount,
+          startDate,
+          rate,
+          endDate,
+        );
 
+        return {
+          loan: loanInterest,
+          buy: buyInterest,
+          sell: sellInterest,
+          diesel: dieselInterest,
+        };
+      });
       return {
-        loan: loanInterest,
-        buy: buyInterest,
-        sell: sellInterest,
-        diesel: dieselInterest,
+        shopeNumber: shopeNumber,
+        transactions: shopeData,
       };
     });
-    return {
-      shopeNumber: shopeNumber,
-      transactions: shopeData,
-    };
-  });
 
-  console.log(allShopes);
+    return res.status(200).json({
+      status: "Success",
+      data: allShopes,
+      Code: "dashbord fetched successfully",
+    });
+  } catch (err) {
+    console.log(err.message);
+    return res.status(500).json({
+      status: "Error",
+      Code: "",
+      message: err.message,
+    });
+  }
 }
 
 module.exports = { dashBordData };
