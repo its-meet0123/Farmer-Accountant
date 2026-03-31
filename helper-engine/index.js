a06ebe8512b2b9288348af5d443afb141d3e26ec
its-meet0123 <sukh.meet9896@gmail.com>
2026-03-31 14:21:30 +0530

    try to solve problem in header options 1

--- helper-engine/index.js
+++ helper-engine/index.js
@@ -28,7 +28,21 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options(cors(corsOptions));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", corsOptions.origin);
+  res.header("Access-Control-Allow-Methods", corsOptions.methods.join(", "));
+  res.header(
+    "Access-Control-Allow-Headers",
+    corsOptions.allowedHeaders.join(", "),
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  next();
+});
 
 app.use(cookieParser());
 app.use(express.json());
