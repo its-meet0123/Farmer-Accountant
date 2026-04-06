0b3ab9f523f784f9bc785f8a53a5c56848944e46
its-meet0123 <sukh.meet9896@gmail.com>
2026-04-06 16:33:19 +0530

    try to set monthly turnover component

--- farmer-heplers/src/app/pages/dashbord/Dashbord.jsx
+++ farmer-heplers/src/app/pages/dashbord/Dashbord.jsx
@@ -6,7 +6,7 @@ import { useLocation } from "react-router-dom";
 import { useAuth } from "../../auth/AuthContext";
 import { styles } from "./DasnbordStyle";
 import FeatureCard from "./FeatureCard";
-//import MonthlyTurnover from "../graph/monthlyTurnover";
+import MonthlyTurnover from "../graph/MonthlyTurnover";
 
 const DashBord = () => {
   const location = useLocation();
@@ -141,8 +141,7 @@ const DashBord = () => {
           <FeatureCard key={index} item={item} isLoading={isLoading} />
         ))}
       </div>
-      {/* <MonthlyTurnover dashbordData={dashbordData} /> */}
-
+      <MonthlyTurnover transactions={dashbordData} />
       <div style={styles.footerBar}></div>
     </div>
   );
---
+++ farmer-heplers/src/app/pages/graph/MonthlyTurnover.jsx
@@ -0,0 +1,6 @@
+const MonthlyTurnover = ({ transactions }) => {
+  console.log("monthly turnover component :", transactions);
+  return <></>;
+};
+
+export default MonthlyTurnover;
