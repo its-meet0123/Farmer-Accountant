7de56370b670e63ebde5fb77b2754f5cc13af56e
its-meet0123 <sukh.meet9896@gmail.com>
2026-04-08 12:31:02 +0530

    set card width 7th times

--- farmer-heplers/src/app/pages/graph/MonthlyTurnover.jsx
+++ farmer-heplers/src/app/pages/graph/MonthlyTurnover.jsx
@@ -17,35 +17,35 @@ const TurnoverGraph = ({ trunover }) => {
   return (
     <>
       {trunover.length != 0 && (
-        <Card title="Turnover Graph" style={{ width: "100%", height: 400 }}>
-          {/* <ResponsiveContainer width="100%" height="100%"> */}
-          <LineChart
-            data={trunover}
-            width={"100%"}
-            height={500}
-            margin={{ top: 10, right: 30, left: 20, bottom: 10 }}>
-            <CartesianGrid strokeDasharray="3 3" vertical={false} />
+        <Card title="Turnover Graph" style={{ width: "100%", height: 600 }}>
+          <ResponsiveContainer width="100%" height="100%">
+            <LineChart
+              data={trunover}
+              width={"100%"}
+              height={"100%"}
+              margin={{ top: 10, right: 30, left: 20, bottom: 10 }}>
+              <CartesianGrid strokeDasharray="3 3" vertical={false} />
 
-            <XAxis dataKey="month" />
+              <XAxis dataKey="month" />
 
-            <YAxis
-              domain={[-300000, 300000]}
-              tickFormatter={(value) => `₹${value.toLocaleString()}`}
-            />
+              <YAxis
+                domain={[-300000, 300000]}
+                tickFormatter={(value) => `₹${value.toLocaleString()}`}
+              />
 
-            <Tooltip formatter={(value) => `₹${value.toLocaleString()}`} />
-            <Legend />
+              <Tooltip formatter={(value) => `₹${value.toLocaleString()}`} />
+              <Legend />
 
-            <ReferenceLine y={0} stroke="#000" strokeWidth={1} label="Zero" />
+              <ReferenceLine y={0} stroke="#000" strokeWidth={1} label="Zero" />
 
-            <Line
-              type="monotone"
-              dataKey="grandTotal"
-              stroke="#1890ff"
-              strokeWidth={3}
-            />
-          </LineChart>
-          {/* </ResponsiveContainer> */}
+              <Line
+                type="monotone"
+                dataKey="grandTotal"
+                stroke="#1890ff"
+                strokeWidth={3}
+              />
+            </LineChart>
+          </ResponsiveContainer>
         </Card>
       )}
     </>
