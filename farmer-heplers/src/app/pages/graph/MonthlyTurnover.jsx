1b73bf918b8964359509a8b11bb0a2c3bcf31a91
its-meet0123 <sukh.meet9896@gmail.com>
2026-04-08 11:25:26 +0530

    set grap in dasbord

--- farmer-heplers/src/app/pages/graph/MonthlyTurnover.jsx
+++ farmer-heplers/src/app/pages/graph/MonthlyTurnover.jsx
@@ -1,10 +1,51 @@
-import { Accessibility } from "lucide-react";
-import { useMemo } from "react";
+import { Card } from "antd";
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  ReferenceLine,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 
 const TurnoverGraph = ({ trunover }) => {
   console.log("monthly turnover component :", trunover);
 
-  return <></>;
+  return (
+    <>
+      <Card title="Turnover Graph">
+        <ResponsiveContainer width="100%" height="100%">
+          <LineChart
+            data={trunover}
+            margin={{ top: 10, right: 30, left: 20, bottom: 10 }}>
+            <CartesianGrid strokeDasharray="3 3" vertical={false} />
+
+            <XAxis dataKey="month" />
+
+            <YAxis
+              domain={[-300000, 300000]}
+              tickFormatter={(value) => `₹${value.toLocaleString()}`}
+            />
+
+            <Tooltip formatter={(value) => `₹${value.toLocaleString()}`} />
+            <Legend />
+
+            <ReferenceLine y={0} stroke="#000" strokeWidth={1} label="Zero" />
+
+            <Line
+              type="monotone"
+              dataKey="grandTotal"
+              stroke="#1890ff"
+              strokeWidth={3}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </Card>
+    </>
+  );
 };
 
 export default TurnoverGraph;
