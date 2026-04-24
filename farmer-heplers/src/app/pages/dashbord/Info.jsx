6f50d6c291df81db7011d0185dea9a3046060609
its-meet0123 <sukh.meet9896@gmail.com>
2026-04-24 07:25:39 +0530

    set style of info dashbord

--- farmer-heplers/src/app/pages/dashbord/Info.jsx
+++ farmer-heplers/src/app/pages/dashbord/Info.jsx
@@ -12,14 +12,14 @@ import { Link } from "react-router-dom";
 
 const DashbordInfo = () => {
   return (
-    <>
+    <div style={{ gap: 2 }}>
       <h3 style={{ color: "#faad14" }}>
         <ArrowRight /> Niche diye gaye vivran ko dhyanpurvak padhein.
       </h3>
       <Row
         gutter={[24, 24]}
         justify="center"
-        style={{ margin: "0 0.2rem 0 0.2rem", gap: 2 }}>
+        style={{ margin: "0 0.2rem 0 0.2rem" }}>
         <Col xs={24} sm={12} md={8}>
           <Card style={glassStyle} hoverable>
             <Store
@@ -140,7 +140,7 @@ const DashbordInfo = () => {
       <Row
         gutter={[24, 24]}
         justify="center"
-        style={{ margin: "0 0.2rem 0 0.2rem", gap: 5 }}>
+        style={{ margin: "0 0.2rem 0 0.2rem" }}>
         <Col xs={24} sm={12} md={8}>
           <Card style={glassStyle} hoverable>
             <Truck
@@ -257,7 +257,7 @@ const DashbordInfo = () => {
           </Card>
         </Col>
       </Row>
-    </>
+    </div>
   );
 };
 
