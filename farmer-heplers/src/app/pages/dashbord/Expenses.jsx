16725f659c9600a00f40aa6c26ef8260ac835398
its-meet0123 <sukh.meet9896@gmail.com>
2026-05-05 06:45:14 +0530

    set icons size

--- farmer-heplers/src/app/pages/dashbord/Expenses.jsx
+++ farmer-heplers/src/app/pages/dashbord/Expenses.jsx
@@ -22,7 +22,7 @@ const Expense = ({ data }) => {
       no: "2",
       label: (
         <>
-          <GiPlantSeed style={{ marginRight: 6 }} />
+          <GiPlantSeed style={{ marginRight: 6 }} size={4} />
           {t("dashbord.cards.table.lt2")}
         </>
       ),
@@ -32,7 +32,7 @@ const Expense = ({ data }) => {
       no: "3",
       label: (
         <>
-          <MdEngineering style={{ marginRight: 6 }} />
+          <MdEngineering style={{ marginRight: 6 }} size={3} />
           {t("dashbord.cards.table.lt3")}
         </>
       ),
@@ -42,7 +42,7 @@ const Expense = ({ data }) => {
       no: "4",
       label: (
         <>
-          <GiFarmTractor style={{ marginRight: 6 }} />
+          <GiFarmTractor style={{ marginRight: 6 }} size={5} />
           {t("dashbord.cards.table.lt4")}
         </>
       ),
