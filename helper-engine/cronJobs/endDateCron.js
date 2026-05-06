90526f1ab00e246567ae89a99ce847b00f056438
its-meet0123 <sukh.meet9896@gmail.com>
2026-05-06 09:04:44 +0530

    set jobs for every second to check performance

--- helper-engine/cronJobs/endDateCron.js
+++ helper-engine/cronJobs/endDateCron.js
@@ -4,7 +4,7 @@ const Workers = require("../models/worker");
 const { calculateAutoInterst } = require("../components/calculator");
 
 const autoCalculationJob = () => {
-  cron.schedule("0 0 * * *", async () => {
+  cron.schedule("* * * * *", async () => {
     try {
       const today = new Date();
       const [shopes, workers] = await Promise.all([
